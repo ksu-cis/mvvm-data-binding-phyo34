de8992d6fd721c7e38344de1d806a7feee83e416
May Phyo <maytphyo@gmail.com>
2020-04-03 16:49:14 -0500

    Done

---
+++ MVVMDataBinding/BillControl.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CashRegister;
+
+namespace MVVMDataBinding
+{
+    /// <summary>
+    /// Interaction logic for BillControl.xaml
+    /// </summary>
+    public partial class BillControl : UserControl
+    {
+        /// <summary>
+        /// Dependency property used to store the denominations of the bills
+        /// </summary>
+        public static readonly DependencyProperty DenominationProperty =
+            DependencyProperty.Register(
+                "Denomination",                     
+                typeof(Bills),                   
+                typeof(BillControl),               
+                new PropertyMetadata(Bills.One)   
+                );
+
+
+        /// <summary>
+        /// Gets and sets the denominations of the bill
+        /// </summary>
+        public Bills Denomination
+        {
+            get { return (Bills)GetValue(DenominationProperty); }
+            set { SetValue(DenominationProperty, value); }
+        }
+
+        /// <summary>
+        /// Dependency property used to store quantity of bills
+        /// </summary>
+        public static readonly DependencyProperty QuantityProperty =
+                DependencyProperty.Register(
+                    "Quantity",
+                    typeof(int),
+                    typeof(BillControl),
+                    new FrameworkPropertyMetadata(
+                        0,
+                        FrameworkPropertyMetadataOptions.BindsTwoWayByDefault
+                    )
+                );
+
+        /// <summary>
+        /// Gets and sets the quantity of the bills
+        /// </summary>
+        public int Quantity
+        {
+            get { return (int)GetValue(QuantityProperty); }
+            set { SetValue(QuantityProperty, value); }
+        }
+
+        /// <summary>
+        /// Increases the quantity 
+        /// </summary>
+        /// <param name="sender">The coinage quanity (as an int)</param>
+        /// <param name="args">The event args</param>
+        public void OnAddClicked(object sender, RoutedEventArgs args)
+        {
+            Quantity++;
+        }
+
+        /// <summary>
+        /// Decreases the quantity 
+        /// </summary>
+        /// <param name="sender">The coinage quantity (as in int)</param>
+        /// <param name="args">The event args</param>
+        public void OnRemoveClicked(object sender, RoutedEventArgs args)
+        {
+            Quantity--;
+        }
+
+
+        public BillControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MVVMDataBinding/CoinControl.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CashRegister;
+
+namespace MVVMDataBinding
+{
+    /// <summary>
+    /// Interaction logic for CoinControl.xaml
+    /// </summary>
+    public partial class CoinControl : UserControl
+    {
+
+        /// <summary>
+        /// Dependency property used to store the denominations of the coins
+        /// </summary>
+        public static readonly DependencyProperty DenominationProperty =
+            DependencyProperty.Register(
+                "Denomination",
+                typeof(Coins),
+                typeof(CoinControl),
+                new PropertyMetadata(Coins.Penny)
+            );
+
+        /// <summary>
+        /// Gets and sets the denomination of the coins
+        /// </summary>
+        public Coins Denomination
+        {
+            get { return (Coins)GetValue(DenominationProperty); }
+            set { SetValue(DenominationProperty, value); }
+        }
+      
+        /// <summary>
+        /// Dependency property used to store quantity of coins
+        /// </summary>
+        public static readonly DependencyProperty QuantityProperty =
+                DependencyProperty.Register(
+                    "Quantity",
+                    typeof(int),
+                    typeof(CoinControl),
+                    new FrameworkPropertyMetadata(
+                        0,
+                        FrameworkPropertyMetadataOptions.BindsTwoWayByDefault
+                    )
+                );
+
+        /// <summary>
+        /// Gets and sets the quantity of coins 
+        /// </summary>
+        public int Quantity
+        {
+            get { return (int)GetValue(QuantityProperty); }
+            set { SetValue(QuantityProperty, value); }
+        }
+        public CoinControl()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Increases the quantity
+        /// </summary>
+        /// <param name="sender">The coinage quanity (as an int)</param>
+        /// <param name="args">The event args</param>
+        public void OnAddClicked(object sender, RoutedEventArgs args)
+        {
+            Quantity++;
+        }
+
+        /// <summary>
+        /// Decreases the quantity 
+        /// </summary>
+        /// <param name="sender">The coinage quantity (as in int)</param>
+        /// <param name="args">The event args</param>
+        public void OnRemoveClicked(object sender, RoutedEventArgs args)
+        {
+            Quantity--;
+        }
+
+    }
+}
